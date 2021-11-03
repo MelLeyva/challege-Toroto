6451ccf1c3d62e5666d965863bc35dc6debe3050
MelLeyva <mleyva0795@gmail.com>
2021-11-03 05:01:31 -0600

    media querys of first view

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import logo from "../../icons/svg/logo.svg";
+import menu from "../../icons/svg/hamburguer_menu_icon.svg"
 import extrLink from "../../icons/svg/external-link.svg";
 import React from "react";
 import "./Header.scss";
@@ -6,19 +7,20 @@ import "./Header.scss";
 function NavBar() {
   return (
     <header>
-    <div className="navbar">
+      <div className="navbar">
+        <img src={menu} className='menu' alt='menu' />
         <img src={logo} className="App-logo" alt="logo" />
-    </div>
-    <span className="links">
-      <ul className="projects">PROYECTOS</ul>
-      <ul className="about">SOBRE TOROTO</ul>
-      <ul className="blog">BLOG</ul>
-      <ul className="meta">
-        <img src={extrLink} className="external-link" alt="external-link" />
-        META REGISTRO
-      </ul>
-      <button href="/contact">CONTACTO</button>
-    </span>
+      </div>
+      <span className="links">
+        <ul className="projects">PROYECTOS</ul>
+        <ul className="about">SOBRE TOROTO</ul>
+        <ul className="blog">BLOG</ul>
+        <ul className="meta">
+          <img src={extrLink} className="external-link" alt="external-link" />
+          META REGISTRO
+        </ul>
+        <button href="/contact">CONTACTO</button>
+      </span>
     </header>
   );
 }
